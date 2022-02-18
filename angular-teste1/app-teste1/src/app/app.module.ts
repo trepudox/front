39dc986c04e7559f58fe2308d9be01a17bca7907
Marco Aurelio Queiroz <teoaa2@gmail.com>
2022-02-18 18:21:26 -0300

    navbar

--- angular-teste1/app-teste1/src/app/app.module.ts
+++ angular-teste1/app-teste1/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SomaComponent } from './soma/soma.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SomaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
