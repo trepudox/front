46c7348823fbd26e6218056ab35ed2dc4cfb4d2b
Marco Aurelio Queiroz <teoaa2@gmail.com>
2022-03-12 12:17:21 -0300

    criando novo app para testes e criando components de binding

---
+++ angular/testes/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InterpolationComponent } from './binding/interpolation/interpolation.component';
+import { PropertyBindingComponent } from './binding/property-binding/property-binding.component';
+import { EventBindingComponent } from './binding/event-binding/event-binding.component';
+import { TwoWayBindingComponent } from './binding/two-way-binding/two-way-binding.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InterpolationComponent,
+    PropertyBindingComponent,
+    EventBindingComponent,
+    TwoWayBindingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
