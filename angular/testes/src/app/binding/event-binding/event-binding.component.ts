e23d7fcc9268e2c8283886e6f0dee5f3e6b9e734
Marco Aurelio Queiroz <teoaa2@gmail.com>
2022-03-12 17:01:01 -0300

    event-binding

--- angular/testes/src/app/binding/event-binding/event-binding.component.ts
+++ angular/testes/src/app/binding/event-binding/event-binding.component.ts
@@ -12,4 +12,17 @@ export class EventBindingComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public position: {x: number, y: number} = {x: 0, y: 0};
+
+  public alerta(msg: string, event: MouseEvent): void {
+    alert(msg + " event logado no console");
+    console.log(event);
+    console.log((event.target as HTMLButtonElement).textContent);
+  }
+
+  public displayPosition(event: MouseEvent): void {
+    this.position.x = event.offsetX;
+    this.position.y = event.offsetY;
+  }
+
 }
